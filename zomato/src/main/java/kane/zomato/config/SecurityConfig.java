d78294eb3f237b0342cc7f4bfbad8995f016ddb9
umar-developer-01 <umarkhandev9@gmail.com>
2026-02-07 19:30:47 +0530

    Chnages in the secuity config

--- zomato/src/main/java/kane/zomato/config/SecurityConfig.java
+++ zomato/src/main/java/kane/zomato/config/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/auth/**",
-                                "/hotel/**",
+//                                "/hotel/**",
 //                                "/user/**",
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**"
